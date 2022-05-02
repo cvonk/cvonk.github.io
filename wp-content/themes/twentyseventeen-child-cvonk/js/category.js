37c80fb25c826e487dec702d66bf820ab2e71203
Coert Vonk <coert.vonk@gmail.com>
2022-05-02 09:46:17 -0700

    clean slate

--- wp-content/themes/twentyseventeen-child-cvonk/js/category.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", function() {
-    jQuery("article").each(function(ii,obj) {
-	obj.onclick = function() {
-            jQuery("article")[ii].setAttribute("data-clicked", "1");
-	};
-    });
-});
